3714d0ebb6359cbcd970f66907a8b64275474658
Remon Tripura <remontripura045@gmail.com>
2024-03-13 17:33:01 +0600

    push

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
 
 
-const date = new Date();
-console.log(date);
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qpfli06.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = `mongodb+srv://biz-server:Rw0EHIzcPbOFIDqq@cluster0.qpfli06.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qpfli06.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -22,20 +18,18 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
-        // await client.connect();
-
         const db = client.db("newsCollection");
-
         const allNews = db.collection("news");
+
+
+
 
         app.post('/news', async (req, res) => {
             const { img, title, news } = req.body;
             const date = new Date().toDateString();
-            console.log(date);
-            const query = { img, title, news, date: currentDate }
+            const query = { img, title, news, date: date }
             const result = await allNews.insertOne(query);
             res.send(result)
         })
@@ -53,13 +47,18 @@ async function run() {
         })
 
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // await client.close();
     }
 }
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log("api is runnig on ", port);
 })
+
+
+
+
+
+
+
